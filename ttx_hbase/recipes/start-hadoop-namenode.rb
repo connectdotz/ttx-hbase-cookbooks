e2f4ea7b84b89209de31bda2838d6818855ef9e3
Vanessa Sun <vsun@connectdotz.com>
2013-10-04 15:42:41 -0400

    fix ruby syntax

--- ttx_hbase/recipes/start-hadoop-namenode.rb
+++ ttx_hbase/recipes/start-hadoop-namenode.rb
@@ -7,12 +7,13 @@ include_recipe "ttx_hbase::hadoop-services"
 ruby_block "format-and-start-namenode" do
 	block do
 		node[:ttx_hbase][:hadoop][:name_dir].each do |dir|
-		if (!File.exists?("#{dir}/current/VERSION"))
-			cmd = "echo 'Y' | #{node[:ttx_hbase][:hadoop][:home]}/bin/hadoop namenode -format"
-			%x( #{cmd} )
-			Chef::Log.info( "namenode is formatted: #{cmd}" )
+			if (!File.exists?("#{dir}/current/VERSION"))
+				cmd = "echo 'Y' | #{node[:ttx_hbase][:hadoop][:home]}/bin/hadoop namenode -format"
+				%x( #{cmd} )
+				Chef::Log.info( "namenode is formatted: #{cmd}" )
+			end
+			break
 		end
-		break
 	end
     notifies :enable, resources(:service => 'hadoop-namenode')
     notifies :restart, resources(:service => 'hadoop-namenode')
